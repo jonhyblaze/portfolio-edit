7f20c05ca26cde8f5dd73b6911fc2330dc459064
Alexander K. <jonhy.blaze@gmail.com>
2026-08-08 20:54:13 +0300

    switching projects order, adding content to footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,21 +1,16 @@
-"use client"
-
 import profile from "@/data/profile"
-import Link from "next/link"
-
 export function Footer() {
   return (
     <footer className="border-t border-muted-foreground dark:border-white/10">
       <div className="flex flex-col justify-between max-w-7xl mx-auto px-5 sm:px-16 laptop:px-0  py-10 pb-10 gap-10 lg:gap-0 lg:flex-row">
-        <h3 className="label-s text-center mx-auto">
+        <h3 className="text-muted-foreground label-s text-center mx-auto">
           Designed by{" "}
-          <Link
+          <a
             href={profile.devPortfolio}
             target="_blank"
-            rel="noopener noreferrer"
-            className="decoration-1 decoration-muted-foreground  underline underline-offset-4">
+            className="decoration-1 decoration-muted-foreground  underline underline-offset-4 hover:text-foreground">
             Devtools 044
-          </Link>{" "}
+          </a>{" "}
           | Kyiv, Ukraine 2026
         </h3>
       </div>
